7c48ded3b7f09097d3565e270f6c8e41e43d98dd
Kiselyov <a6.kiselyov@itransition.com>
2021-03-13 17:49:57 +0300

    resolve configs

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,19 @@ import express, { Application, Request, Response } from 'express';
 import someData from "./Utils/SomeData";
 
 const port: number = 5000;
-const app: Application = express();
+const index: Application = express();
 
 const add = (a: number, b: number): number => a + b;
 
-app.get('/members', (req: Request, res: Response) => {
+index.get('/', (req: Request, res: Response) => {
   const response: string = JSON.stringify({ data: someData, add: add(5, 6) });
   res.send(response);
 })
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('response');
-})
+// index.get('/', (req: Request, res: Response) => {
+//   res.send('response');
+// })
 
-app.listen(port, () => {
+index.listen(port, () => {
   console.log(`Server running on port ${port}`);
 })
