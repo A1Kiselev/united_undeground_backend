1f67f68b30e2f2250efe320c654ebe7ae15783be
Kiselyov <a6.kiselyov@itransition.com>
2021-03-13 14:55:56 +0300

    fixing configs to setup on Versel

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import someData from "./Utils/SomeData";
 
-const port: number = 5001;
+const port: number = 5000;
 const app: Application = express();
 
 const add = (a: number, b: number): number => a + b;
@@ -9,6 +9,10 @@ const add = (a: number, b: number): number => a + b;
 app.get('/members', (req: Request, res: Response) => {
   const response: string = JSON.stringify({ data: someData, add: add(5, 6) });
   res.send(response);
+})
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('response');
 })
 
 app.listen(port, () => {
